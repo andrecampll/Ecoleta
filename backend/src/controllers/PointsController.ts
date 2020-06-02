4f42b4a127e610443ca504a8818d9d6a4572af8e
André Victor <andrevictor50@gmail.com>
2020-06-02 11:32:18 -0300

    🎉 finished api in video

--- backend/src/controllers/PointsController.ts
+++ backend/src/controllers/PointsController.ts
@@ -17,7 +17,7 @@ class PointsController {
     const trx = await knex.transaction();
 
     const point = {
-      image: 'fake',
+      image: 'https://images.unsplash.com/photo-1542838132-92c53300491e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
       name,
       email,
       whatsapp,
