ffc669570823b046bd446af0b30a39f659d258d9
André Victor <andrevictor50@gmail.com>
2020-06-03 10:34:51 -0300

    🗺️  added map

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
+import { Map, TileLayer, Marker } from 'react-leaflet';
 
 import { Container } from './styles';
 
@@ -49,6 +50,15 @@ const SignUp: React.FC = () => {
             <h2>Endereço</h2>
             <span>Selecione o endereço no mapa</span>
           </legend>
+
+          <Map center={[-1.4515086,-48.4487568]} zoom={15}>
+            <TileLayer
+              attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+
+            <Marker position={[-1.4515086,-48.4487568]} />
+          </Map>
 
           <div className="field-group">
             <div className="field">
