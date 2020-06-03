9915f7735a6ed1aa0f24ec9d43f75ed2935feb06
André Victor <andrevictor50@gmail.com>
2020-06-03 12:04:50 -0300

    🎉 finished app web

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState, useCallback, ChangeEvent } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState, useCallback, ChangeEvent, FormEvent } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Map, TileLayer, Marker } from 'react-leaflet';
 
@@ -44,6 +44,8 @@ const SignUp: React.FC = () => {
   const [selectedCity, setSelectedCity] = useState('0');
   const [selectedItens, setSelectedItens] = useState<number[]>([]);  
   const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
+
+  const history = useHistory();
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(position => {
@@ -113,6 +115,35 @@ const SignUp: React.FC = () => {
     }
   }, [selectedItens]);
 
+  const handleSubmit = useCallback(async (e : FormEvent) => {
+    e.preventDefault();
+
+    const { email, name, whatsapp } = formData;
+
+    const uf = selectedUF;
+    const city = selectedCity;
+    const [latitude, longitude] = selectedPosition;
+
+    const itens = selectedItens;
+
+    const data = {
+      name,
+      email,
+      whatsapp,
+      uf,
+      city,
+      latitude,
+      longitude,
+      itens
+    };
+
+    await api.post('points', data);
+
+    alert('Ponto de coleta criado!');
+
+    history.push('/');
+  }, [formData, history, selectedCity, selectedItens, selectedPosition, selectedUF]);
+
   return (
     <Container>
       <header>
@@ -124,7 +155,7 @@ const SignUp: React.FC = () => {
         </Link>
       </header>
 
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <h1>Cadastro do <br /> ponto de coleta</h1>
       
         <fieldset>
