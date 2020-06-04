868e46e23bbd50fa0b138c48b0a4642dfc200ae2
André Victor <andrevictor50@gmail.com>
2020-06-04 10:33:35 -0300

    🚀 added Detail page

--- mobile/src/pages/Detail/index.tsx
+++ mobile/src/pages/Detail/index.tsx
@@ -1,10 +1,47 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
-
-// import { Container } from './styles';
+import React, { useCallback } from 'react';
+import { View, StyleSheet, TouchableOpacity, Image, Text, SafeAreaView } from 'react-native';
+import { Feather as Icon, FontAwesome } from '@expo/vector-icons'
+import { useNavigation } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler';
 
 const Detail: React.FC = () => {
-  return <View />;
+  const navigation = useNavigation();
+
+  const handleNavigateBack = useCallback(() => {
+    navigation.goBack();
+  }, []);
+
+  return (
+    <SafeAreaView style={{ flex: 1 }} >
+      <View style={styles.container} >
+        <TouchableOpacity onPress={handleNavigateBack}>
+          <Icon name="arrow-left" color="#34CB79" size={24} />
+        </TouchableOpacity>
+
+        <Image style={styles.pointImage} source={{ uri: 'https://images.unsplash.com/photo-1542838132-92c53300491e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60' }}  />
+      
+        <Text style={styles.pointName}>Mercado do João</Text>
+        <Text style={styles.pointItems}>Lâmpadas, Óleo de Cozinha</Text>
+
+        <View style={styles.address} >
+          <Text style={styles.addressTitle}>Endereços</Text>
+          <Text style={styles.addressContent}>Belém, PA</Text>
+        </View>
+      </View>
+
+      <View style={styles.footer}>
+        <RectButton style={styles.button} onPress={() => {}}>
+          <FontAwesome name="whatsapp" size={20} color="#fff" />
+          <Text style={styles.buttonText}>Whatsapp</Text>
+        </RectButton>
+
+        <RectButton style={styles.button} onPress={() => {}}>
+          <Icon name="mail" size={20} color="#fff" />
+          <Text style={styles.buttonText}>E-mail</Text>
+        </RectButton>
+      </View>
+    </SafeAreaView>
+  )
 }
 
 export default Detail;
