3bb728fa1b25241a6dcefa757e5c3a52ae4812eb
André Victor <andrevictor50@gmail.com>
2020-06-05 09:58:46 -0300

    🚀 added uploads feature on web

--- web/src/components/Header/index.tsx
+++ web/src/components/Header/index.tsx
@@ -1,13 +1,32 @@
 import React from 'react';
+import Switch from 'react-switch';
+import { ThemeContext } from 'styled-components';
 
 import logo from '../../assets/logo.svg';
 
 import { Container } from './styles';
 
+interface Props {
+  toggleTheme(): void,
+}
+
 const Header: React.FC = () => {
   return (
     <header>
       <img src={logo} alt="Ecoleta"/>
+
+      {/* <Switch
+        onChange={toggleTheme}
+        checked={title === 'dark'}
+        checkedIcon={false}
+        uncheckedIcon={false}
+        height={10}
+        width={40}
+        handleDiameter={20}
+        offColor="#ccc"
+        onColor="#333"
+      /> */}
+
     </header>
   );
 }
--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -8,6 +8,8 @@ import { LeafletMouseEvent } from 'leaflet';
 import axios from 'axios';
 import api from '../../services/api';
 
+import Dropzone from '../../components/Dropzone';
+
 import { Container } from './styles';
 
 import logo from '../../assets/logo.svg';
@@ -44,6 +46,7 @@ const SignUp: React.FC = () => {
   const [selectedCity, setSelectedCity] = useState('0');
   const [selectedItens, setSelectedItens] = useState<number[]>([]);  
   const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
+  const [selectedFile, setSelectedFile] = useState<File>();
 
   const history = useHistory();
 
@@ -126,28 +129,32 @@ const SignUp: React.FC = () => {
 
     const itens = selectedItens;
 
-    const data = {
-      name,
-      email,
-      whatsapp,
-      uf,
-      city,
-      latitude,
-      longitude,
-      itens
-    };
+    const data = new FormData();
+
+    data.append('name', name);
+    data.append('email', email);
+    data.append('whatsapp', whatsapp);
+    data.append('uf', uf);
+    data.append('city', city);
+    data.append('latitude', String(latitude));
+    data.append('longitude', String(longitude));
+    data.append('itens', itens.join(','));
+
+    if (selectedFile) {
+      data.append('image', selectedFile);
+    }
 
     await api.post('points', data);
 
     alert('Ponto de coleta criado!');
 
     history.push('/');
-  }, [formData, history, selectedCity, selectedItens, selectedPosition, selectedUF]);
+  }, [formData, history, selectedCity, selectedFile, selectedItens, selectedPosition, selectedUF]);
 
   return (
     <Container>
       <header>
-        <img src={logo} alt="Ecoleta"/>
+        <img src={logo} alt="Ecoleta"/>''
       
         <Link to="/">
           <FiArrowLeft />
@@ -157,7 +164,9 @@ const SignUp: React.FC = () => {
 
       <form onSubmit={handleSubmit}>
         <h1>Cadastro do <br /> ponto de coleta</h1>
-      
+
+        <Dropzone onFileUploaded={setSelectedFile} />
+
         <fieldset>
           <legend>
             <h2>Dados</h2>
